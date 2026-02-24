b38cc0f51aac9f9eeb468e8f67dea9cd1d8950f1
Your Name <you@example.com>
2026-02-24 18:28:12 +0100

    test: implement spec runner test for muxed_encode

--- packages/core-dart/test/spec_runner_test.dart
+++ packages/core-dart/test/spec_runner_test.dart
@@ -1,7 +1,30 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:test/test.dart';
+import 'package:stellar_address_kit/stellar_address_kit.dart';
 
 void main() {
-  test('Vector tests', () {
-    // Read vectors.json and run tests...
+  final vectorsFile = File('../../spec/vectors.json');
+  if (!vectorsFile.existsSync()) {
+    print('Warning: vectors.json not found at ${vectorsFile.absolute.path}');
+    return;
+  }
+  
+  final vectorsJson = jsonDecode(vectorsFile.readAsStringSync());
+  final cases = vectorsJson['cases'] as List;
+
+  group('Vector tests', () {
+    for (final c in cases) {
+      if (c['module'] == 'muxed_encode') {
+        test('[muxed_encode] ${c['description']}', () {
+          final gAddress = c['input']['gAddress'] as String;
+          final id = BigInt.parse(c['input']['id'] as String);
+          final expectedM = c['expected']['mAddress'] as String;
+
+          final result = MuxedAddress.encode(baseG: gAddress, id: id);
+          expect(result, equals(expectedM));
+        });
+      }
+    }
   });
 }
